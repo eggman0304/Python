582780f2cd0361bf1cae77a37d9a799b60932d46
Ray <raylin@aten.com>
2020-11-06 16:16:41 +0800

    1.test

--- openCV_video.py
+++ openCV_video.py
@@ -2,9 +2,10 @@ import numpy as np
 import cv2
 
 # 播放影片
-cap = cv2.VideoCapture(r'D:/Python_code/opencv/影片.mov')
+#cap = cv2.VideoCapture(r'D:/Python_code/opencv/影片.mov')
 # 讀取IP Camera內容
 #cap = cv2.VideoCapture('http://192.168.0.137:81/videostream.cgi?loginuse=username&loginpas=password&resolution=32')
+cap = cv2.VideoCapture('rtsp://admin:@@aten168@10.0.128.82:554/')
 # 以迴圈從影片檔案讀取影格，並顯示出來
 while cap.isOpened():
     ret, frame = cap.read()
@@ -12,16 +13,17 @@ while cap.isOpened():
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    lower_blue=np.array([20,180,210])
-    upper_blue=np.array([120,215,255])
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    lower_blue=np.array([0,43,46])
+    upper_blue=np.array([180,255,255])
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
     mask=cv2.inRange(hsv,lower_blue,upper_blue)
     res=cv2.bitwise_and(frame,frame,mask=mask)
-    cv2.imshow("res",mask)
+    #cv2.imshow("res",res)
     #cv2.imshow('frame', gray)
-    #cv2.imshow('frame', frame)
-    if cv2.waitKey(10) == ord('q'):
+    cv2.imshow('frame', frame)
+    cv2.imwrite("result1.jpg",frame)
+    if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -6,14 +6,16 @@ def pick_color(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         pixel = image[y,x]
         #you might want to adjust the ranges(+-10, etc):
-        upper =  np.array([pixel[0] + 10, pixel[1] + 10, pixel[2] + 10])
-        lower =  np.array([pixel[0] - 10, pixel[1] - 10, pixel[2] - 10])
+        upper =  np.array([pixel[0] + 20, pixel[1] + 30, pixel[2] + 20])
+        lower =  np.array([pixel[0] - 20, pixel[1] - 30, pixel[2] - 20])
         print(pixel, lower, upper)
         image_mask = cv2.inRange(image,lower,upper)
         cv2.imshow("mask",image_mask)
-image = cv2.imread(r'D:/Python_code/opencv/1.jpg')
+image = cv2.imread(r'D:/Python_code/opencv/result1.jpg')
 cv2.namedWindow('Image')
 cv2.setMouseCallback('Image', pick_color)
 cv2.imshow("Image",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
